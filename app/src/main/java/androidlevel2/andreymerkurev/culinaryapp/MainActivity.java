852c53d8d904cccf505b850f9eb26bc562985d12
andreyprogrammer <andreyprogrammer@mail.ru>
2019-12-09 00:56:22 +0300

    create empty project

---
+++ app/src/main/java/androidlevel2/andreymerkurev/culinaryapp/MainActivity.java
@@ -0,0 +1,14 @@
+package androidlevel2.andreymerkurev.culinaryapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
